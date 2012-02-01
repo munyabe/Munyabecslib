c2e2a375c6b0b99651a83966bc4454c4b4ca71c2
yo.betek@gmail.com <yo.betek@gmail.com@47c62297-2159-9c3e-8ad9-4e328270ee9a>
2012-02-01 15:28:10 +0000

    Added Trim methods.
    git-svn-id: http://munyabecslib.googlecode.com/svn/trunk/MunyabeCSLibrary@47 47c62297-2159-9c3e-8ad9-4e328270ee9a

--- Munyabe.Common/StringExtensions.cs
+++ Munyabe.Common/StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Munyabe.Common
 {
@@ -12,8 +14,11 @@ namespace Munyabe.Common
         /// </summary>
         /// <param name="value">値を抽出する文字列</param>
         /// <returns>抽出した大文字の文字列</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/>が<see langword="null"/>です。</exception>
         public static string GetUpperCases(this string value)
         {
+            Guard.ArgumentNotNull(value, "value");
+
             if (string.IsNullOrWhiteSpace(value))
             {
                 return string.Empty;
@@ -21,6 +26,53 @@ namespace Munyabe.Common
 
             var chars = value.Where(char.IsUpper);
             return string.Concat(chars);
+        }
+
+        /// <summary>
+        /// 文字列の先頭および末尾から指定された文字列を削除します。
+        /// </summary>
+        /// <param name="value">削除対象の文字列</param>
+        /// <param name="trimString">削除する文字列</param>
+        /// <returns>指定された文字列を削除した後に残った文字列</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/>が<see langword="null"/>です。</exception>
+        public static string Trim(this string value, string trimString)
+        {
+            return TrimHelper(value, trimString, "^({0})+|({0})+$");
+        }
+
+        /// <summary>
+        /// 文字列の末尾から指定された文字列を削除します。
+        /// </summary>
+        /// <param name="value">削除対象の文字列</param>
+        /// <param name="trimString">削除する文字列</param>
+        /// <returns>指定された文字列を削除した後に残った文字列</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/>が<see langword="null"/>です。</exception>
+        public static string TrimEnd(this string value, string trimString)
+        {
+            return TrimHelper(value, trimString, "({0})+$");
+        }
+
+        /// <summary>
+        /// 文字列の先頭から指定された文字列を削除します。
+        /// </summary>
+        /// <param name="value">削除対象の文字列</param>
+        /// <param name="trimString">削除する文字列</param>
+        /// <returns>指定された文字列を削除した後に残った文字列</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/>が<see langword="null"/>です。</exception>
+        public static string TrimStart(this string value, string trimString)
+        {
+            return TrimHelper(value, trimString, "^({0})+");
+        }
+
+        /// <summary>
+        /// 文字列から指定されたパターンに一致する文字列を削除するヘルパーメソッドです。
+        /// </summary>
+        private static string TrimHelper(string value, string trimString, string patternFormat)
+        {
+            Guard.ArgumentNotNull(value, "value");
+
+            var pattern = string.Format(patternFormat, Regex.Escape(trimString));
+            return Regex.Replace(value, pattern, string.Empty);
         }
     }
 }
--- Test.Munyabe.Common/StringExtensionsTest.cs
+++ Test.Munyabe.Common/StringExtensionsTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Munyabe.Common;
 
 namespace Test.Munyabe.Common
@@ -16,6 +17,52 @@ namespace Test.Munyabe.Common
             Assert.AreEqual("ET", "stringExtensionsTest".GetUpperCases());
             Assert.AreEqual("EXT", "stringEXtensionsTest".GetUpperCases());
             Assert.AreEqual(string.Empty, "string".GetUpperCases());
+        }
+
+        [TestMethod]
+        public void TrimTest()
+        {
+            Assert.AreEqual("Value", "TypeValue".Trim("Type"));
+            Assert.AreEqual("Value", "TypeTypeValue".Trim("Type"));
+            Assert.AreEqual("Value", "{0}Value".Trim("{0}"));
+
+            Assert.AreEqual("Type", "TypeValue".Trim("Value"));
+            Assert.AreEqual("Type", "TypeValueValue".Trim("Value"));
+            Assert.AreEqual("Type", "Type{0}".Trim("{0}"));
+
+            Assert.AreEqual("Type", "Type".Trim("Value"));
+            Assert.AreEqual("Type", "Type".Trim(string.Empty));
+            Assert.AreEqual(string.Empty, "Value".Trim("Value"));
+            Assert.AreEqual(string.Empty, string.Empty.Trim("Value"));
+
+            Assert.AreEqual("Type", "ValueTypeValue".Trim("Value"));
+            Assert.AreEqual("Type", "{0}Type{0}".Trim("{0}"));
+        }
+
+        [TestMethod]
+        public void TrimEndTest()
+        {
+            Assert.AreEqual("Type", "TypeValue".TrimEnd("Value"));
+            Assert.AreEqual("Type", "TypeValueValue".TrimEnd("Value"));
+            Assert.AreEqual("Type", "Type".TrimEnd("Value"));
+            Assert.AreEqual("Type", "Type".TrimEnd(string.Empty));
+            Assert.AreEqual(string.Empty, "Value".TrimEnd("Value"));
+            Assert.AreEqual(string.Empty, string.Empty.TrimEnd("Value"));
+
+            Assert.AreEqual("Type", "Type{0}".TrimEnd("{0}"));
+        }
+
+        [TestMethod]
+        public void TrimStartTest()
+        {
+            Assert.AreEqual("Value", "TypeValue".TrimStart("Type"));
+            Assert.AreEqual("Value", "TypeTypeValue".TrimStart("Type"));
+            Assert.AreEqual("Type", "Type".TrimStart("Value"));
+            Assert.AreEqual("Type", "Type".TrimStart(string.Empty));
+            Assert.AreEqual(string.Empty, "Value".TrimStart("Value"));
+            Assert.AreEqual(string.Empty, string.Empty.TrimStart("Type"));
+
+            Assert.AreEqual("Value", "{0}Value".TrimStart("{0}"));
         }
     }
 }
