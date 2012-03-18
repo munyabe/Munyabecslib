ba8e9c713b07e22220cb36bd0df7c13f13e53b08
yo.betek@gmail.com <yo.betek@gmail.com@47c62297-2159-9c3e-8ad9-4e328270ee9a>
2012-03-18 12:28:06 +0000

    Added ParallelQueryExtensions class.
    git-svn-id: http://munyabecslib.googlecode.com/svn/trunk/MunyabeCSLibrary@55 47c62297-2159-9c3e-8ad9-4e328270ee9a

---
+++ Munyabe.Common/ParallelQueryExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Munyabe.Common;
+
+namespace Isid.Istyle
+{
+    /// <summary>
+    /// <see cref="ParallelQuery{T}"/>の拡張メソッドを定義するクラスです。
+    /// </summary>
+    public static class ParallelQueryExtensions
+    {
+        /// <summary>
+        /// Parallel LINQ で実行されるスレッドに現在のカルチャを設定します。
+        /// </summary>
+        /// <typeparam name="TSource"><paramref name="source"/>の要素の型</typeparam>
+        /// <param name="source">対象の<see cref="ParallelQuery{T}"/></param>
+        /// <returns>現在のスレッドが設定された<see cref="ParallelQuery{T}"/></returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/>が<see langword="null"/>です。</exception>
+        public static ParallelQuery<TSource> AsCurrentCulture<TSource>(this ParallelQuery<TSource> source)
+        {
+            Guard.ArgumentNotNull(source, "source");
+
+            var currentCulture = Thread.CurrentThread.CurrentCulture;
+            return source.Select(each =>
+                {
+                    Thread.CurrentThread.CurrentCulture = currentCulture;
+                    Thread.CurrentThread.CurrentUICulture = currentCulture;
+                    return each;
+                });
+        }
+    }
+}
