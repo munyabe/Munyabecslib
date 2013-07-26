203cb987ee9276fcdfd0242127d94e70e6d625d0
yo.betek@gmail.com <yo.betek@gmail.com@47c62297-2159-9c3e-8ad9-4e328270ee9a>
2013-07-26 01:49:31 +0000

    Added RectExtensions class.
    git-svn-id: http://munyabecslib.googlecode.com/svn/trunk/MunyabeCSLibrary@72 47c62297-2159-9c3e-8ad9-4e328270ee9a

---
+++ Munyabe.Windows/RectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Munyabe.Windows
+{
+    /// <summary>
+    /// <see cref="Rect"/>の拡張メソッドを定義するクラスです。
+    /// </summary>
+    public static class RectExtensions
+    {
+        /// <summary>
+        /// 四角形の中心座標を取得します。
+        /// </summary>
+        /// <param name="rect">四角形</param>
+        /// <returns>中心座標</returns>
+        public static Point Center(this Rect rect)
+        {
+            return new Point((rect.Left + rect.Right) / 2, (rect.Top + rect.Bottom) / 2);
+        }
+    }
+}
---
+++ Test.Munyabe.Windows/RectExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Munyabe.Windows;
+
+namespace Test.Munyabe.Windows
+{
+    [TestClass]
+    public class RectExtensionsTest
+    {
+        [TestMethod]
+        public void CenterTest()
+        {
+            var rect = new Rect(200, 100, 400, 300);
+            var center = rect.Center();
+
+            Assert.AreEqual(400, center.X);
+            Assert.AreEqual(250, center.Y);
+        }
+    }
+}
