7d90c6e1bdc2272f9546ced24c2173bd0e82c1c8
munyabe <yo.betek@gmail.com>
2014-06-03 20:12:17 +0900

    Added BitOperationUtil class.

---
+++ Munyabe.Common/BitOperationUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Munyabe.Common
+{
+    /// <summary>
+    /// ビット演算に関するユーティリティクラスです。
+    /// </summary>
+    public static class BitOperationUtil
+    {
+        /// <summary>
+        /// 整数をビットの数列に分解します。
+        /// </summary>
+        /// <remarks>
+        /// ex) 13 = 1 + 4 + 8
+        /// </remarks>
+        /// <param name="num">分解する整数</param>
+        /// <returns>ビットの数列</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><param name="num"/>が負の整数です。</exception>
+        public static IEnumerable<int> DivideToBits(int num)
+        {
+            if (num < 0)
+            {
+                throw new ArgumentOutOfRangeException(string.Format("The argument [num] must be over 0."));
+            }
+
+            for (int i = 0; 0 < num; num >>= 1, i++)
+            {
+                if ((num & 1) != 0)
+                {
+                    yield return 1 << i;
+                }
+            }
+        }
+    }
+}
---
+++ Test.Munyabe.Common/BitOperationUtilTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Munyabe.Common;
+
+namespace Test.Munyabe.Common
+{
+    /// <summary>
+    /// <see cref="BitOperationUtil"/>のテストクラスです。
+    /// </summary>
+    [TestClass]
+    public class BitOperationUtilTest
+    {
+        [TestMethod]
+        public void DivideToBitsTest()
+        {
+            CollectionAssert.AreEqual(new[] { 1 }, BitOperationUtil.DivideToBits(1).ToArray());
+            CollectionAssert.AreEqual(new[] { 2, 4 }, BitOperationUtil.DivideToBits(6).ToArray());
+            CollectionAssert.AreEqual(new[] { 8 }, BitOperationUtil.DivideToBits(8).ToArray());
+            CollectionAssert.AreEqual(new[] { 1, 4, 8 }, BitOperationUtil.DivideToBits(13).ToArray());
+
+            Assert.IsFalse(BitOperationUtil.DivideToBits(0).Any());
+        }
+    }
+}
