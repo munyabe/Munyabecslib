f87ebfc7d478112e4f8bde157d834c24b7441859
munyabe <yo.betek@gmail.com>
2014-10-06 19:31:21 +0900

    Added FontUtil class.

---
+++ Munyabe.Windows/FontUtil.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Windows.Media;
+
+namespace Munyabe.Windows
+{
+    /// <summary>
+    /// フォントに関するユーティリティクラスです。
+    /// </summary>
+    public static class FontUtil
+    {
+        /// <summary>
+        /// システムにインストールされているフォントかどうかを判定します。
+        /// </summary>
+        /// <param name="fontName">フォント名</param>
+        /// <returns>インストールされている場合は<see langword="true"/></returns>
+        public static bool IsInstalledFont(string fontName)
+        {
+            return Fonts.SystemFontFamilies.Any(family => family.FamilyNames.Values.Contains(fontName));
+        }
+    }
+}
---
+++ Test.Munyabe.Windows/FontUtilTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Windows.Media;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Munyabe.Windows;
+
+namespace Test.Munyabe.Windows
+{
+    /// <summary>
+    /// <see cref="FontUtil"/>のテストクラスです。
+    /// </summary>
+    [TestClass]
+    public class FontUtilTest
+    {
+        [TestMethod]
+        public void IsInstalledFontTest()
+        {
+            Assert.IsTrue(FontUtil.IsInstalledFont("Arial"));
+            Assert.IsTrue(FontUtil.IsInstalledFont("Meiryo"));
+
+            var meiryo = new FontFamily("Meiryo");
+            Assert.IsTrue(meiryo.FamilyNames.Values.All(FontUtil.IsInstalledFont));
+
+            Assert.IsFalse(FontUtil.IsInstalledFont("Hoge"));
+            Assert.IsFalse(FontUtil.IsInstalledFont("ほげ"));
+
+            Assert.IsFalse(FontUtil.IsInstalledFont(string.Empty));
+            Assert.IsFalse(FontUtil.IsInstalledFont(null));
+        }
+    }
+}
