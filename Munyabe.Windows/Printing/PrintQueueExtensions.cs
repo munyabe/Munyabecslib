a9c4eff324d3ec827ca45c016c656ce2418f9b57
munyabe <yo.betek@gmail.com>
2014-10-20 11:17:20 +0900

    Added PrintQueue extension method.

---
+++ Munyabe.Windows/Printing/PrintQueueExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Printing;
+using Munyabe.Common;
+
+namespace Munyabe.Windows.Printing
+{
+    /// <summary>
+    /// <see cref="PrintQueue"/>の拡張メソッドを定義するクラスです。
+    /// </summary>
+    public static class PrintQueueExtensions
+    {
+        /// <summary>
+        /// プリンターのドライバーが Microsoft XPS Document Writer かどうかを判定します。
+        /// </summary>
+        /// <remarks>
+        /// Microsoft XPS Document Writer では<see cref="PrintQueue.IsXpsDevice"/>は<see langword="false"/>となるため判別できません。
+        /// ※ 参考
+        /// http://stackoverflow.com/questions/5043761/intercepting-printdialog-to-xps-document-writer%EF%BC%89
+        /// </remarks>
+        /// <param name="source">判定する印刷のキュー</param>
+        /// <returns>Microsoft XPS Document Writer の場合は<see langword="true"/></returns>
+        public static bool IsXpsDocumentWriter(this PrintQueue source)
+        {
+            Guard.ArgumentNotNull(source, "source");
+
+            // MEMO : 他に適切な判別方法が無いため、ドライバ名で判別します。
+            return source.QueueDriver.Name == "Microsoft XPS Document Writer";
+        }
+    }
+}
---
+++ Test.Munyabe.Windows/Printing/PrintQueueExtensionsTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing.Printing;
+using System.Linq;
+using System.Printing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Munyabe.Common;
+using Munyabe.Windows.Printing;
+
+namespace Test.Munyabe.Windows.Printing
+{
+    /// <summary>
+    /// <see cref="PrintQueueExtensions"/>のテストクラスです。
+    /// </summary>
+    [TestClass]
+    public class PrintQueueExtensionsTest
+    {
+        [TestMethod]
+        public void IsXpsDocumentWriterTest()
+        {
+            var server = new PrintServer();
+            PrinterSettings.InstalledPrinters
+                .Cast<string>()
+                .Select(name => new PrintQueue(server, name))
+                .ForEach(queue =>
+                {
+                    if (queue.QueueDriver.Name == "Microsoft XPS Document Writer")
+                    {
+                        Assert.IsTrue(queue.IsXpsDocumentWriter());
+                    }
+                    else
+                    {
+                        Assert.IsFalse(queue.IsXpsDocumentWriter());
+                    }
+                });
+        }
+    }
+}
