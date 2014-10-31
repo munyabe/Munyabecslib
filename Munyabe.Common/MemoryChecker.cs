7e7f23ba6f72d5cb07729a5664bf268b4023db0d
munyabe <yo.betek@gmail.com>
2014-10-31 20:57:11 +0900

    Added MemoryChecker class.

---
+++ Munyabe.Common/MemoryChecker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Munyabe.Common
+{
+    /// <summary>
+    /// メモリーリークをチェックするための機能を提供するクラスです。
+    /// </summary>
+    public static class MemoryChecker
+    {
+        /// <summary>
+        /// メモリーリークをチェックする対象のインスタンスへの弱参照です。
+        /// </summary>
+        private static readonly IList<WeakReference> _checkObjects = new List<WeakReference>();
+
+        /// <summary>
+        /// GC を実行し、登録されたインスタンスで参照が保持されているインスタンスを列挙します。
+        /// </summary>
+        /// <remarks>
+        /// ここで返却されるオブジェクトがメモリーリークしている可能性があります。
+        /// </remarks>
+        /// <returns>参照が保持されているインスタンス</returns>
+        public static IEnumerable<object> GetAliveObjects()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            lock (_checkObjects)
+            {
+                for (int i = 0; i < _checkObjects.Count; i++)
+                {
+                    var weakRef = _checkObjects[i];
+                    object target = weakRef.Target;
+                    if (target != null)
+                    {
+                        yield return target;
+                    }
+                    else
+                    {
+                        _checkObjects.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// GC によるクリアの対象になっていると考えられるインスタンスを登録します。
+        /// </summary>
+        /// <param name="target">対象のインスタンス</param>
+        /// <returns>対象のインスタンスへの弱参照</returns>
+        public static WeakReference RegisterCheckObject(object target)
+        {
+            if (target == null)
+            {
+                return null;
+            }
+
+            var weakRef = new WeakReference(target);
+            lock (_checkObjects)
+            {
+                _checkObjects.Add(weakRef);
+            }
+
+            return weakRef;
+        }
+    }
+}
---
+++ Test.Munyabe.Common/MemoryCheckerTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Munyabe.Common;
+
+namespace Test.Munyabe.Common
+{
+    /// <summary>
+    /// <see cref="MemoryChecker"/>のテストクラスです。
+    /// </summary>
+    [TestClass]
+    public class MemoryCheckerTest
+    {
+        [TestMethod]
+        public void GetAliveObjectsTest()
+        {
+            var target1 = new LeakedTarget();
+            MemoryChecker.RegisterCheckObject(target1);
+
+            var target2 = new LeakedTarget();
+            MemoryChecker.RegisterCheckObject(target2);
+
+            var results = MemoryChecker.GetAliveObjects().ToList();
+            Assert.AreEqual(2, results.Count);
+            Assert.IsTrue(results.Contains(target1));
+            Assert.IsTrue(results.Contains(target2));
+
+            target1 = null;
+            results = null;
+
+            results = MemoryChecker.GetAliveObjects().ToList();
+            Assert.AreEqual(1, results.Count);
+            Assert.IsTrue(results.Contains(target2));
+
+            target2 = null;
+            results = null;
+
+            results = MemoryChecker.GetAliveObjects().ToList();
+            Assert.AreEqual(0, results.Count);
+        }
+
+        private sealed class LeakedTarget
+        { }
+    }
+}
